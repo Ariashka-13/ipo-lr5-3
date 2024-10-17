1471b3fa540313d277ffbd257ce86cc75b6c431a
Ariashka <ariana0tsarova@gmail.com>
2024-10-17 18:22:19 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-myfile=open("D:\\profiles\\TsarevA_81\\Desktop\\ипо\\лр 5\\3\\text.txt", "r") #открыть файл
+myfile=open("text.txt", "r") #открыть файл
 words = 0 #счетчик количества слов
-with open("D:\\profiles\\TsarevA_81\\Desktop\\ипо\\лр 5\\3\\text.txt", "r") as myfile: #для оборачивания выполнения блока инструкций
+with open("text.txt", "r") as myfile: #для оборачивания выполнения блока инструкций
     for i in myfile:#использование цикла
         words=len(i.split()) #подсчет количества слов
 print("Количество слов: ", words) #вывод количества слов
